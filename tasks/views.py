a61581feb9bc039d83d8f74471a8373e4bc04cdd
yovidev <yovanis.suarez.s@gmail.com>
2024-07-21 21:26:52 -0500

    Commit inicial

---
+++ tasks/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from django.db import IntegrityError
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    
+    if request.method == 'GET':
+        return render(request,'signup.html',{
+            'form':UserCreationForm
+        })
+        
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            
+            except IntegrityError:
+                return render(request,'signup.html',{
+                    'form':UserCreationForm,
+                    'error': 'Usuario existente'
+                 })
+            
+        else:
+            return render(request,'signup.html',{
+                    'form':UserCreationForm,
+                    'error': 'Contrasennas incorrectas'
+                 })
+        
+        
+def tasks(request):
+    return render(request, 'tasks.html')
+    
+def signout(request):
+    logout(request)
+    return redirect('home')         
+
+
+def signin(request):
+    return render(request, 'signin.html')      
+                
