0a079eea62607091ea33f3fa0dbb63a04b52abb2
yovidev <yovanis.suarez.s@gmail.com>
2024-07-27 22:47:54 -0500

    actualizado 27-7-24

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -44,7 +44,23 @@ def tasks(request):
     tasks = Task.objects.filter(user = request.user)
     return render(request, 'tasks.html',{'tasks':tasks})
 
-
+def task_detail(request,task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request,'task_detail.html',
+                    {'task':task, 'form': form})
+        
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request,'task_detail.html',
+                    {'task':task, 'form': form, 
+                     'error': "Error actualizando tarea"})
 
 def create_task(request):
     if request.method == 'GET':
@@ -91,5 +107,5 @@ def signin(request):
         
 
             
-  ## Tiempo del video 1h.44m            
+             
                 
